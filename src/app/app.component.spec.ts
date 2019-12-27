ea294aa8a72bbb38206c628c61bcb8a256143050
Nahuel Bentos <nahuel.bentos@fing.edu.uy>
2019-12-27 16:27:08 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-perf'`, () => {
+  it(`should have as title 'platzi-store'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-perf');
+    expect(app.title).toEqual('platzi-store');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-perf app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('platzi-store app is running!');
   });
 });
